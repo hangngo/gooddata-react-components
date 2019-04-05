0cfeaad5616e313911ef53847573aa13ef3f0a17
Hang Ngo <hang.ngo@gooddata.com>
2019-04-05 10:42:50 +0700

    filter measure

--- examples/src/components/MeasureFilterExample.jsx
+++ examples/src/components/MeasureFilterExample.jsx
@@ -7,36 +7,70 @@ import '@gooddata/react-components/styles/css/main.css';
 
 import { totalSalesIdentifier, monthDateIdentifier, projectId } from '../utils/fixtures';
 
+const filterPresets = {
+    attributeCalifornia: {
+        label: 'Attribute (California)',
+        key: 'attributeCalifornia',
+        filterItem: Model.positiveAttributeFilter(
+            locationStateDisplayFormIdentifier,
+            [locationStateAttributeCaliforniaUri]
+        )
+    },
+    lastYear: {
+        label: 'Last year',
+        key: 'lastYear',
+        filterItem: Model.relativeDateFilter(dateDatasetIdentifier, 'GDC.time.year', -1, -1)
+    },
+    noData: {
+        label: 'No Data',
+        key: 'noData',
+        filterItem: Model.relativeDateFilter(dateDatasetIdentifier, 'GDC.time.year', 1, 1)
+    },
+    franchiseFeesCalifornia: {
+        label: 'Franchise Fees California',
+        key: 'franchiseFeesCalifornia',
+        filterItem: null
+    }
+};
+
+const franchiseFeesCalifornia =
+    Model.measure(franchiseFeesIdentifier)
+        .localIdentifier('franchiseFeesCalifornia')
+        .alias('FranchiseFees (California)')
+        .filters(filterPresets.attributeCalifornia.filterItem);
+
 export class MeasureFilterExample extends Component {
     render() {
         return (
             <div style={{ height: 300 }} className="s-measure-filter">
             <ColumnChart
                 projectId={projectId}
-                measures={[{
-                    measure: {
-                        localIdentifier: 'totalSales',
-                        definition: {
-                            measureDefinition: {
-                                item: {
-                                    identifier: 'aa7ulGyKhIE5'
-                                },
-                                filters: [
-                                    {
-                                        positiveAttributeFilter: {
-                                            displayForm: {
-                                                identifier: 'label.restaurantlocation.locationstate'
-                                            },
-                                            in: ['Florida', 'Texas'],
-                                            textFilter: true
-                                        }
-                                    }
-                                ],
-                            }
-                        },
-                        alias: 'Location State'
-                    }
-                }]}
+                franchiseFeesCalifornia
+                // measures={[{
+                //     measure: {
+                //         localIdentifier: 'totalSales',
+                //         definition: {
+                //             measureDefinition: {
+                //                 item: {
+                //                     identifier: 'aa7ulGyKhIE5'
+                //                 },
+                //                 filters: [
+                //                     {
+                //                         positiveAttributeFilter: {
+                //                             displayForm: {
+                //                                 identifier: 'label.restaurantlocation.locationstate'
+                //                             },
+                //                             in: ['Florida', 'Texas'],
+                //                             textFilter: true
+                //                         }
+                //                     }
+                //                 ],
+                //             }
+                //         },
+                //         alias: 'Location State'
+                //     }
+                // }]}
+                
             />
         </div>
         );
